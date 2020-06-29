3b7d92d5540277f9865485a6f7bcfe7577cfd331
sam <samreality4@gmail.com>
2020-06-29 12:11:50 -0700

    finish matrix

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,57 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+const results = [];
+for (let i=0; i < n; i++){
+    results.push([]);
+}
+//   n = amountOfSubArray
+
+    
+    let counter = 1;
+    //this keep track of the current number inside of those array
+
+    let startColumn = 0;
+    let startRow = 0;
+
+   
+    let endColumn = n-1;
+    let endRow = n-1;
+     //predefined where to end
+
+    while(startColumn <= endColumn && startRow <= endRow){
+        //Top row
+        for(let i = startColumn; i<= endColumn; i++){
+            results[startRow][i] = counter;
+            counter++;
+        }
+        startRow++;
+
+        //Right Column
+        for(let i = startRow; i<= endRow; i++){
+            results[i][endColumn] = counter;
+            counter++;
+        }
+
+        endColumn--;
+
+        //Bottow row
+        for(let i = endColumn; i >=startColumn; i--){
+            results[endRow][i] = counter;
+            counter++;
+        }
+        endRow--;
+        //Start Column
+        for (let i = endRow; i>=startRow; i--){
+            results[i][startColumn] = counter;
+            counter++;
+        }
+        startColumn++;
+    }
+
+return results;
+
+}
 
 module.exports = matrix;
